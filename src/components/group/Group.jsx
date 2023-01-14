343e88074512f22165992fd5e29e077b69957320
khubaibahmed69 <72744627+khubaibahmed69@users.noreply.github.com>
2023-01-14 11:53:33 +0500

    make the group draggable

--- src/components/group/Group.jsx
+++ src/components/group/Group.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import {useDrop} from "react-dnd";
 import GroupProduct from "./GroupProduct";
+import Draggable, {DraggableCore} from "react-draggable";
 // eslint-disable-next-line no-unused-vars
 import styles from "./Group.css";
 
@@ -12,33 +13,35 @@ export default function Group({id, name, products, addProductToGroup, changeName
     const [groupName, setGroupName] = useState(name);
 
     return (
-        <div className="group-container">
-            <input
-                className="group-name"
-                value={groupName}
-                onChange={({target}) => {
-                    setGroupName(target.value);
-                }}
-                onBlur={({target}) => {
-                    changeName(target.value);
-                }}
-                placeholder="Group name..."
-            ></input>
+        <Draggable handle=".handle">
+            <div className="group-container">
+                <input
+                    className="group-name handle"
+                    value={groupName}
+                    onChange={({target}) => {
+                        setGroupName(target.value);
+                    }}
+                    onBlur={({target}) => {
+                        changeName(target.value);
+                    }}
+                    placeholder="Group name..."
+                ></input>
 
-            <div className="group" ref={drop}>
-                <input className="group-header" value={"121 unit, $1200"}></input>
-                <div className="group-content">
-                    {products &&
-                        products.map(product => (
-                            <GroupProduct
-                                key={product.id}
-                                currentGroupId={id}
-                                productInfo={product}
-                                onViewDetails={() => onViewDetails(product.id)}
-                            />
-                        ))}
+                <div className="group" ref={drop}>
+                    <input className="group-header handle" value={"121 unit, $1200"}></input>
+                    <div className="group-content">
+                        {products &&
+                            products.map(product => (
+                                <GroupProduct
+                                    key={product.id}
+                                    currentGroupId={id}
+                                    productInfo={product}
+                                    onViewDetails={() => onViewDetails(product.id)}
+                                />
+                            ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </Draggable>
     );
 }
