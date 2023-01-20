26a43219c5ed62a3d0cf58c82720d2a2237db148
khubaibahmed69 <72744627+khubaibahmed69@users.noreply.github.com>
2023-01-20 14:54:05 +0500

    add ZoomIn functionality

--- src/components/group/Group.jsx
+++ src/components/group/Group.jsx
@@ -2,8 +2,10 @@ import React, {useState} from "react";
 import {useDrop} from "react-dnd";
 import GroupProduct from "./GroupProduct";
 import Draggable, {DraggableCore} from "react-draggable";
+import {FormControlLabel, Paper, Switch, Zoom} from "@mui/material";
 // eslint-disable-next-line no-unused-vars
 import styles from "./Group.css";
+import BasicModal from "../Modal/GroupModal";
 
 export default function Group({id, name, products, addProductToGroup, changeName, onViewDetails}) {
     const [{isOver}, drop] = useDrop(() => ({
@@ -11,38 +13,71 @@ export default function Group({id, name, products, addProductToGroup, changeName
         drop: item => addProductToGroup(item.currentGroupId, item.id, id)
     }));
     const [groupName, setGroupName] = useState(name);
+    const [checked, setChecked] = React.useState(false);
+
+    const handleCheckedChange = () => {
+        setChecked(prev => !prev);
+    };
 
     return (
-        <Draggable handle=".handle">
-            <div className="group-container">
-                <input
-                    className="group-name handle"
-                    value={groupName}
-                    onChange={({target}) => {
-                        setGroupName(target.value);
-                    }}
-                    onBlur={({target}) => {
-                        changeName(target.value);
-                    }}
-                    placeholder="Group name..."
-                ></input>
+        <>
+            {checked && (
+                <BasicModal
+                    setChecked={setChecked}
+                    divElement={
+                        <div className="group-content">
+                            {products &&
+                                products.map((product, _index) => (
+                                    <GroupProduct
+                                        key={product.id}
+                                        currentGroupId={id}
+                                        productInfo={product}
+                                        onViewDetails={() => onViewDetails(product.id)}
+                                        index={_index}
+                                        zoomedIn={true}
+                                    />
+                                ))}
+                        </div>
+                    }
+                ></BasicModal>
+            )}
+            <Draggable handle=".handle">
+                <div className="group-container">
+                    <input
+                        style={{width: `${groupName.length * 7}px`}}
+                        className="group-name handle"
+                        value={groupName}
+                        onChange={({target}) => {
+                            setGroupName(target.value);
+                        }}
+                        onBlur={({target}) => {
+                            changeName(target.value);
+                        }}
+                        placeholder="Group"
+                    ></input>
 
-                <div className="group" ref={drop}>
-                    <input className="group-header handle" value={"121 unit, $1200"}></input>
-                    <div className="group-content">
-                        {products &&
-                            products.map((product, _index) => (
-                                <GroupProduct
-                                    key={product.id}
-                                    currentGroupId={id}
-                                    productInfo={product}
-                                    onViewDetails={() => onViewDetails(product.id)}
-                                    index={_index}
-                                />
-                            ))}
+                    <div className="group" ref={drop}>
+                        <input className="group-header handle" value={"121 unit, $1200"}></input>
+                        <div className="group-content">
+                            {products &&
+                                products.map((product, _index) => (
+                                    <GroupProduct
+                                        key={product.id}
+                                        currentGroupId={id}
+                                        productInfo={product}
+                                        onViewDetails={() => onViewDetails(product.id)}
+                                        index={_index}
+                                        zoomedIn={false}
+                                    />
+                                ))}
+                        </div>
                     </div>
+                    <FormControlLabel
+                        control={<Switch checked={checked} onChange={handleCheckedChange} />}
+                        label="Zoom In"
+                    />
                 </div>
-            </div>
-        </Draggable>
+            </Draggable>
+        </>
     );
 }
--- src/components/group/GroupProduct.jsx
+++ src/components/group/GroupProduct.jsx
@@ -1,9 +1,9 @@
-import { Scale } from "@mui/icons-material";
+import {Scale} from "@mui/icons-material";
 import {color} from "@mui/system";
 import React from "react";
 import {useDrag} from "react-dnd";
 
-export default function GroupProduct({productInfo, currentGroupId, onViewDetails, index}) {
+export default function GroupProduct({productInfo, currentGroupId, onViewDetails, index, zoomedIn}) {
     const [{isDragging}, drag] = useDrag(() => ({
         type: "product",
         item: {id: productInfo.id, currentGroupId},
@@ -14,6 +14,13 @@ export default function GroupProduct({productInfo, currentGroupId, onViewDetails
 
     const marginArr = [0, 1, 2, 1];
     const height = 140;
+    const marginLeft = () => {
+        if (zoomedIn) return "0px";
+        else {
+            if (index === 0) return "0px";
+            else return `-${height - 10}px`;
+        }
+    };
     return (
         <img
             alt="draggable pic"
@@ -22,12 +29,13 @@ export default function GroupProduct({productInfo, currentGroupId, onViewDetails
             className="group-product"
             src={"/product-images/" + productInfo.image_name}
             style={{
+                transform: zoomedIn ? "scale(1)": "scale(0.5)",
                 height: `${height}px`,
                 width: "auto",
                 border: isDragging ? "10 px solid blue" : "0px solid black",
-                marginLeft: index === 0 ? "0px" : `-${height - 10}px`,
+                marginLeft: marginLeft(),
                 zIndex: index + 1,
-                "margin-top": `${marginArr[index % 4] * 50 - 25}px`
+                "margin-top": zoomedIn ? "100px" : `${marginArr[index % 4] * 50 - 25}px`
             }}
             onClick={() => onViewDetails(productInfo.id)}
         />
