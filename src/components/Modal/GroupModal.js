51bc02172e369882155acf9e7f3d1baaeff0599e
khubaibahmed69 <72744627+khubaibahmed69@users.noreply.github.com>
2023-01-20 14:52:56 +0500

    add groupModal to be used for zoomIn functionality

---
+++ src/components/Modal/GroupModal.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 800,
+    height: 500,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+export default function BasicModal({setChecked, divElement}) {
+    const [open, setOpen] = React.useState(true);
+    //   const handleOpen = () => setOpen(true);
+    const handleClose = () => {
+        setOpen(false)
+        setChecked(false)
+    };
+
+    return (
+        <div>
+            {/* <Button onClick={handleOpen}>Open modal</Button> */}
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    {divElement}
+                </Box>
+            </Modal>
+        </div>
+    );
+}
