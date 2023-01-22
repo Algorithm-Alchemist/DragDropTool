30339acbc7059b5cd1846f7e38ed9a6fd5e969bb
khubaibahmed69 <72744627+khubaibahmed69@users.noreply.github.com>
2023-01-22 21:33:57 +0500

    Update index.jsx

--- src/components/group/GroupContent/index.jsx
+++ src/components/group/GroupContent/index.jsx
@@ -1,8 +1,18 @@
 import GroupProduct from "../GroupProduct";
 
 export default function GroupContent({ products, id, isZoomedIn }) {
+  const _style = isZoomedIn
+    ? {
+        marginTop: "15px",
+        borderBlock: "inherit",
+        display: "flex",
+        flexWrap: "wrap",
+        height: "auto",
+      }
+    : {};
   return (
-    <div className="group-content">
+    <div className="group-content" style={_style}>
+      {" "}
       {products &&
         products.map((product, _index) => (
           <GroupProduct
@@ -11,7 +21,7 @@ export default function GroupContent({ products, id, isZoomedIn }) {
             productInfo={product}
             // onViewDetails={() => onViewDetails(product.id)}
             index={_index}
-            zoomedIn={isZoomedIn}
+            isZoomedIn={isZoomedIn}
           />
         ))}
     </div>
