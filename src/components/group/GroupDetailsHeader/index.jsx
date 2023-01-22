6e7caa000577f773171bb611b94d376ee95dd964
khubaibahmed69 <72744627+khubaibahmed69@users.noreply.github.com>
2023-01-22 17:45:50 +0500

    rename files inside sub-components to index.js

--- src/components/group/GroupDetailsHeader/index.jsx
+++ src/components/group/GroupDetailsHeader/index.jsx
@@ -1,8 +1,17 @@
 export default function GroupDetailsHeader(props) {
   const unit_or_Units = props.noOfUnits>1 ? 'units': 'unit'
+  const _style = props.isZoomedIn
+  ? {
+      fontSize: "medium",
+      margin: '5px',
+      padding: '5px',
+      width: '120px'
+    }
+  : {};
   return (
     <input
       className="group-header handle"
+      style={_style}
       value={`${props.noOfUnits} ${unit_or_Units}, $${props.totalGroupPrice}`}
     ></input>
   );
---
+++ src/components/group/GroupName/index.jsx
@@ -0,0 +1,26 @@
+export default function GroupName(props) {
+  const _style = props.isZoomedIn
+    ? {
+        width: `${props.groupName.length * 9}px`,
+        fontSize: "medium",
+        maxWidth: "300px",
+        minWidth: "90px",
+        padding: '5px',
+        margin: '5px'
+      }
+    : { width: `${props.groupName.length * 7}px` };
+  return (
+    <input
+      className="group-name handle"
+      style={_style}
+      value={props.groupName}
+      onChange={({ target }) => {
+        props.setGroupName(target.value);
+      }}
+      onBlur={({ target }) => {
+        props.setGroupName(target.value);
+      }}
+      placeholder="group name"
+    ></input>
+  );
+}
