9cfe6bc74275cd0a89c649e37e35230d54015a90
khubaibahmed69 <72744627+khubaibahmed69@users.noreply.github.com>
2023-02-23 00:19:52 +0500

    Update Group css and jsx files

--- src/components/group/Group.jsx
+++ src/components/group/Group.jsx
@@ -9,7 +9,13 @@ import GroupName from "./GroupName";
 import GroupDetailsHeader from "./GroupDetailsHeader";
 import GroupZoomFunction from "./GroupZoomFunction";
 
-export default function Group({ id, products, addProductToGroup }) {
+export default function Group({
+  id,
+  products,
+  addProductToGroup,
+  removedProductId,
+  setRemovedProductId,
+}) {
   const [{ isOver }, drop] = useDrop(() => ({
     accept: "product",
     drop: (item) => addProductToGroup(item.currentGroupId, item.id, id),
@@ -21,10 +27,29 @@ export default function Group({ id, products, addProductToGroup }) {
     setChecked((prev) => !prev);
   };
 
+  const [productQuantities, setProductQuantities] = React.useState({});
+
+  React.useEffect(() => {
+    const productQuantitiesTemporary = { ...productQuantities };
+    for (const product of products) {
+      const { id } = product;
+      if (!productQuantitiesTemporary[id]) {
+        productQuantitiesTemporary[id] = 1;
+      }
+    }
+    productQuantitiesTemporary[removedProductId] = 0;
+    setRemovedProductId(0);
+    setProductQuantities(productQuantitiesTemporary);
+  }, [products]);
+
   let totalGroupPrice = 0;
   products.forEach((product) => {
-    totalGroupPrice = totalGroupPrice + product.price;
+    const quantity = productQuantities[product.id] || 1;
+    totalGroupPrice = totalGroupPrice + product.price * quantity;
   });
+
+  let totalQuantity = 0;
+  totalQuantity = Object.values(productQuantities).reduce((a, b) => a + b, 0);
 
   return (
     <>
@@ -39,11 +64,17 @@ export default function Group({ id, products, addProductToGroup }) {
                 isZoomedIn={true}
               />
               <GroupDetailsHeader
-                noOfUnits={products.length}
+                noOfUnits={totalQuantity}
                 totalGroupPrice={totalGroupPrice}
                 isZoomedIn={true}
               />
-              <GroupContent products={products} id={id} isZoomedIn={true} />
+              <GroupContent
+                products={products}
+                id={id}
+                isZoomedIn={true}
+                setProductQuantity={setProductQuantities}
+                productQuantities={productQuantities}
+              />
             </>
           }
         />
@@ -54,10 +85,16 @@ export default function Group({ id, products, addProductToGroup }) {
           <GroupName groupName={groupName} setGroupName={setGroupName} />
           <div className="group" ref={drop}>
             <GroupDetailsHeader
-              noOfUnits={products.length}
+              noOfUnits={totalQuantity}
               totalGroupPrice={totalGroupPrice}
             />
-            <GroupContent products={products} id={id} isZoomedIn={false} />
+            <GroupContent
+              products={products}
+              id={id}
+              isZoomedIn={false}
+              setProductQuantity={setProductQuantities}
+              productQuantities={productQuantities}
+            />
           </div>
           <GroupZoomFunction checked={checked} onChange={handleCheckedChange} />
         </div>
