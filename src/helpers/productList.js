a19daec731df1cf64de59cd51bf895f54d48f563
khubaibahmed69 <72744627+khubaibahmed69@users.noreply.github.com>
2023-02-23 00:22:17 +0500

    Update productList.js

--- src/helpers/productList.js
+++ src/helpers/productList.js
@@ -1,95 +1,95 @@
 export const productList = [
-    {
-      id: 1,
-      name: "Lakers White Jersey 76",
-      image_name: "Lakers White Jersey 76.jpg",
-      price: 23.5,
-    },
-    {
-      id: 2,
-      name: "Lakers Purpule Jersy 33",
-      image_name: "Lakers Purpule Jersy 33.jpg",
-      price: 24.7,
-    },
-    {
-      id: 3,
-      name: "Lakers Blue Jersey",
-      image_name: "Lakers Blue Jersey.webp",
-      price: 25.36,
-    },
-    {
-      id: 4,
-      name: "Dallas Cowboys Blue Hat",
-      image_name: "Dallas Cowboys Blue Hat.jpg",
-      price: 15.5,
-    },
-    {
-      id: 5,
-      name: "Dallas Cowboys White Hat.jpg",
-      image_name: "Dallas Cowboys White Hat.jpg",
-      price: 17.5,
-    },
-  
-    {
-      id: 6,
-      name: "Dallas Cowboys Beeny",
-      image_name: "Dallas Cowboys Beeny .jpg",
-      price: 8.5,
-    },
-  
-    {
-      id: 7,
-      name: "Stance socks 1",
-      image_name: "Stance socks 1.jpg",
-      price: 22.5,
-    },
-    {
-      id: 8,
-      name: "Stance Socks 2",
-      image_name: "Stance Socks 2.jpg",
-      price: 23.5,
-    },
-  
-    {
-      id: 9,
-      name: "Stance Socks 3",
-      image_name: "Stance Socks 3.jpg",
-      price: 23.5,
-    },
-    {
-      id: 10,
-      name: "Stance Socks 4",
-      image_name: "Stance Socks 4.jpg",
-      price: 23.5,
-    },
-  
-    {
-      id: 11,
-      name: "Satorison Shoes 3",
-      image_name: "Satorison Shoes 3.webp",
-      price: 30.5,
-    },
-  
-    {
-      id: 12,
-      name: "Satorison Shoes 1",
-      image_name: "Satorison Shoes 1.jpg",
-      price: 33.5,
-    },
-  
-    {
-      id: 13,
-      name: "Satorison Shoes 2",
-      image_name: "Satorison Shoes 2.jpg",
-      price: 35.75,
-    },
-  
-    { id: 14, name: "Vans Low Top", image_name: "Vans Low Top.jpg", price: 23.5 },
-  
-    {
-      id: 15,
-      name: "Vans High Top",
-      image_name: "Vans High Top.jpg",
-      price: 23.5,
-    },
-  ];
+  {
+    id: 1,
+    name: "Lakers White Jersey 76",
+    image_name: "Lakers White Jersey 76.jpg",
+    price: 23.5,
+  },
+  {
+    id: 2,
+    name: "Lakers Purpule Jersy 33",
+    image_name: "Lakers Purpule Jersy 33.jpg",
+    price: 24.7,
+  },
+  {
+    id: 3,
+    name: "Lakers Blue Jersey",
+    image_name: "Lakers Blue Jersey.webp",
+    price: 25.36,
+  },
+  {
+    id: 4,
+    name: "Dallas Cowboys Blue Hat",
+    image_name: "Dallas Cowboys Blue Hat.jpg",
+    price: 15.5,
+  },
+  {
+    id: 5,
+    name: "Dallas Cowboys White Hat",
+    image_name: "Dallas Cowboys White Hat.jpg",
+    price: 17.5,
+  },
+
+  {
+    id: 6,
+    name: "Dallas Cowboys Beeny",
+    image_name: "Dallas Cowboys Beeny .jpg",
+    price: 8.5,
+  },
+
+  {
+    id: 7,
+    name: "Stance socks 1",
+    image_name: "Stance socks 1.jpg",
+    price: 22.5,
+  },
+  {
+    id: 8,
+    name: "Stance Socks 2",
+    image_name: "Stance Socks 2.jpg",
+    price: 23.5,
+  },
+
+  {
+    id: 9,
+    name: "Stance Socks 3",
+    image_name: "Stance Socks 3.jpg",
+    price: 23.5,
+  },
+  {
+    id: 10,
+    name: "Stance Socks 4",
+    image_name: "Stance Socks 4.jpg",
+    price: 23.5,
+  },
+
+  {
+    id: 11,
+    name: "Satorison Shoes 3",
+    image_name: "Satorison Shoes 3.webp",
+    price: 30.5,
+  },
+
+  {
+    id: 12,
+    name: "Satorison Shoes 1",
+    image_name: "Satorison Shoes 1.jpg",
+    price: 33.5,
+  },
+
+  {
+    id: 13,
+    name: "Satorison Shoes 2",
+    image_name: "Satorison Shoes 2.jpg",
+    price: 35.75,
+  },
+
+  { id: 14, name: "Vans Low Top", image_name: "Vans Low Top.jpg", price: 23.5 },
+
+  {
+    id: 15,
+    name: "Vans High Top",
+    image_name: "Vans High Top.jpg",
+    price: 23.5,
+  },
+];
